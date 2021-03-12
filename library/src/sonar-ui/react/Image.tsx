d6f5e660bc2152e7df18b826bc4dd971b857e071
Matt <matthieu.jabbour@gmail.com>
2021-03-12 11:12:58 +0100

    Improve UIImage behaviour

--- library/src/sonar-ui/react/Image.tsx
+++ library/src/sonar-ui/react/Image.tsx
@@ -21,8 +21,20 @@ const defaultProps = {
 };
 
 const getDimensions = (ratio: string): { width: number; height: number; } => {
-  const dimensions = ratio.split(':');
-  return { width: parseInt(dimensions[0], 10), height: parseInt(dimensions[1], 10) };
+  let dimensions;
+  switch (ratio) {
+    case 'square':
+      return { width: 1, height: 1 };
+    case 'portait':
+      return { width: 2, height: 3 };
+    case 'landscape':
+      return { width: 3, height: 2 };
+    case 'panoramic':
+      return { width: 16, height: 9 };
+    default:
+      dimensions = ratio.split(':').map(parseInt);
+      return { width: dimensions[0], height: dimensions[1] };
+  }
 };
 
 /**
@@ -32,9 +44,24 @@ export default function UIImage(props: InferProps<typeof propTypes>): JSX.Elemen
   // eslint-disable-next-line object-curly-newline
   const { src, alt, ratio, modifiers, itemProp } = props;
   const dimensions = getDimensions(ratio);
-
   const className = buildClass('ui-image', `${ratio.replace(':', 'x')} ${modifiers}`.split(' '));
 
+  // Custom aspect ratio...
+  if (/^([0-9]+):([0-9]+)$/i.test(ratio) === true) {
+    return (
+      <img
+        src={src}
+        alt={alt}
+        loading="lazy"
+        className={className}
+        width={dimensions.width}
+        height={dimensions.height}
+        itemProp={(itemProp !== null) ? itemProp : undefined}
+      />
+    );
+  }
+
+  // Standard aspect ratio...
   return (
     <div className={className}>
       <img
